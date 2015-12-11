d1d8641b37add0d8eca237ba71424e99dadc14c1
Cort Harlow <cort.harlow@gmail.com>
2015-12-11 11:31:15 -0500

    Forgot favicon

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 // Use routes
 app.use(articleRoutes);
